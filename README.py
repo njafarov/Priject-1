31fa2d7fcc8d3dc62196c02de26418f3f408a57b
Nahid Jafarov <60370184+njafarov@users.noreply.github.com>
2021-06-04 02:07:12 +0200

    Update README.py

--- README.py
+++ README.py
@@ -1,14 +1,17 @@
 # Project-1 MNIST
-Alice, Bob, and Daniel are friends learning machine learning together. After watching a few lectures, they are very proud of having learned many useful tools, including linear and logistic regression, non-linear features, regularization, and kernel tricks. To see how these methods can be used to solve a real life problem, they decide to get their hands dirty with the famous digit recognition problem using the MNIST (Mixed National Institute of Standards and Technology) database.
+"""Cross Validation for MNIST data
 
-The MNIST database contains binary images of handwritten digits commonly used to train image processing systems. The digits were collected from among Census Bureau employees and high school students. The database contains 60,000 training digits and 10,000 testing digits, all of which have been size-normalized and centered in a fixed-size image of 28 × 28 pixels. Many methods have been tested with this dataset and in this project, you will get a chance to experiment with the task of classifying these images into the correct digit using some of the methods you have learned so far.
+The MNIST database contains binary images of handwritten digits commonly used to train image processing systems. The digits were collected from among Census Bureau 
+employees and high school students. The database contains 60,000 training digits and 10,000 testing digits, all of which have been size-normalized and centered in a 
+fixed-size image of 28 × 28 pixels. Many methods have been tested with this dataset and in this project, you will get a chance to experiment with the task of 
+classifying these images into the correct digit using some of the methods you have learned so far."""
 
 
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.base import clone
 
-"""Cross Validation for MNIST data"""
+""""""
 
 y_train_5 = (y_train==5) #selects 5 from train data
 y_test_5 = (y_test==5) #selects 5 from test data
